ae3e1fee015cd3297fa667812f35c634e0087038
Lars <udb5td@virginia.edu>
2024-04-28 13:35:49 -0400

    initial commit

---
+++ svm.py
@@ -0,0 +1,15 @@
+################
+# SVM Assignment 
+################
+
+# Import libraries
+import pandas as pd
+import numpy as np
+import scickit-learn as sk
+
+
+# Preprocess data
+sun_data = pd.read_csv('SunData.csv')
+moon_data = pd.read_csv('MoonData.csv')
+
+
