e2cdee7a2df5048a169e487e6a929313ab26bad8
Lars <udb5td@virginia.edu>
2024-04-30 18:11:24 -0400

    init 2

--- svm.py
+++ svm.py
@@ -5,11 +5,22 @@
 # Import libraries
 import pandas as pd
 import numpy as np
-import scickit-learn as sk
+import sklearn as sk
+from sklearn import svm
 
 
-# Preprocess data
 sun_data = pd.read_csv('SunData.csv')
 moon_data = pd.read_csv('MoonData.csv')
+
+clf = sk.svm.SVC()
+clf.fit(sun_data, moon_data)
+clf.support_vectors_
+
+
+
+clf.predict(sun_data)
+sun_data.to_csv('SunDataPredictions.csv')
+clf.predict(moon_data)
+moon_data.to_csv('MoonDataPredictions.csv')
 
 
